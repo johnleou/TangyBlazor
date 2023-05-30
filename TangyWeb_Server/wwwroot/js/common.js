4b6bc32175a8be72f95dd0c510b1ee34c0482b58
Ioannis Leousis <flexcore07@RFB115>
2023-05-30 17:41:11 +0300

    Changes 30/05/2023

---
+++ TangyWeb_Server/wwwroot/js/common.js
@@ -0,0 +1,25 @@
+﻿window.ShowToastr = (type, message) => {
+    if (type === "success") {
+        toastr.success(message, "Operation Successful" ,{ timeΟut: 5000});
+    }
+    if (type === "error") {
+        toastr.error(message, "Operation Failed" ,{ timeΟut: 5000 });
+    }
+}
+
+window.ShowSwal = (type, message) => {
+    if (type === "success") {
+        Swal.fire(
+            'Success Notification!',
+            message,
+            'success'
+        )
+    }
+    if (type === "error") {
+        Swal.fire(
+            'Error Notification!',
+            message,
+            'error'
+        )
+    }
+}
